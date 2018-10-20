a115aad861ebc8eb792e513591a32864c9a67e9a
Faraz Ahmed Khan <Faraz Ahmed Khan@DESKTOP-KMHBSVQ>
2018-10-20 16:46:53 +0500

    Started

---
+++ TutorManagementSystem/LoginScreen.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace TutorManagementSystem
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserNameBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UserNameBox
+            // 
+            this.UserNameBox.Font = new System.Drawing.Font("Century Gothic", 7.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNameBox.ForeColor = System.Drawing.Color.Silver;
+            this.UserNameBox.Location = new System.Drawing.Point(79, 152);
+            this.UserNameBox.Name = "UserNameBox";
+            this.UserNameBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.UserNameBox.Size = new System.Drawing.Size(265, 22);
+            this.UserNameBox.TabIndex = 0;
+            this.UserNameBox.Text = " Enter your Username/NIC/Phone here";
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Font = new System.Drawing.Font("Century Gothic", 7.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordBox.ForeColor = System.Drawing.Color.Silver;
+            this.PasswordBox.Location = new System.Drawing.Point(79, 184);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.PasswordBox.Size = new System.Drawing.Size(265, 22);
+            this.PasswordBox.TabIndex = 1;
+            this.PasswordBox.Text = " Enter your Password here";
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.BackColor = System.Drawing.Color.DodgerBlue;
+            this.LoginBtn.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginBtn.Location = new System.Drawing.Point(79, 216);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(265, 26);
+            this.LoginBtn.TabIndex = 2;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = false;
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 471);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.UserNameBox);
+            this.Name = "LoginScreen";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.LoginScreen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserNameBox;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Button LoginBtn;
+    }
+}
+
