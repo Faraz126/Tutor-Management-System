41fd26cf15e31d288c7b39686e7e3c90bbee167c
Faraz Ahmed Khan <Faraz Ahmed Khan@DESKTOP-KMHBSVQ>
2018-10-20 20:14:29 +0500

    Made changes to visuals of button and text box

--- TutorManagementSystem/LoginScreen.Designer.cs
+++ TutorManagementSystem/LoginScreen.Designer.cs
@@ -35,41 +35,49 @@
             // 
             // UserNameBox
             // 
-            this.UserNameBox.Font = new System.Drawing.Font("Century Gothic", 7.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UserNameBox.ForeColor = System.Drawing.Color.Silver;
+            this.UserNameBox.Font = new System.Drawing.Font("Century Gothic", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNameBox.ForeColor = System.Drawing.Color.DimGray;
             this.UserNameBox.Location = new System.Drawing.Point(79, 152);
             this.UserNameBox.Name = "UserNameBox";
             this.UserNameBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.UserNameBox.Size = new System.Drawing.Size(265, 22);
-            this.UserNameBox.TabIndex = 0;
+            this.UserNameBox.TabIndex = 1;
+            this.UserNameBox.TabStop = false;
             this.UserNameBox.Text = " Enter your Username/NIC/Phone here";
             // 
             // PasswordBox
             // 
-            this.PasswordBox.Font = new System.Drawing.Font("Century Gothic", 7.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PasswordBox.ForeColor = System.Drawing.Color.Silver;
+            this.PasswordBox.Font = new System.Drawing.Font("Century Gothic", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordBox.ForeColor = System.Drawing.Color.DimGray;
             this.PasswordBox.Location = new System.Drawing.Point(79, 184);
             this.PasswordBox.Name = "PasswordBox";
             this.PasswordBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.PasswordBox.Size = new System.Drawing.Size(265, 22);
             this.PasswordBox.TabIndex = 1;
+            this.PasswordBox.TabStop = false;
             this.PasswordBox.Text = " Enter your Password here";
             // 
             // LoginBtn
             // 
             this.LoginBtn.BackColor = System.Drawing.Color.DodgerBlue;
+            this.LoginBtn.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+            this.LoginBtn.FlatAppearance.BorderSize = 2;
+            this.LoginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LoginBtn.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginBtn.ForeColor = System.Drawing.Color.White;
             this.LoginBtn.Location = new System.Drawing.Point(79, 216);
             this.LoginBtn.Name = "LoginBtn";
-            this.LoginBtn.Size = new System.Drawing.Size(265, 26);
+            this.LoginBtn.Size = new System.Drawing.Size(265, 36);
             this.LoginBtn.TabIndex = 2;
             this.LoginBtn.Text = "Login";
             this.LoginBtn.UseVisualStyleBackColor = false;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
             // 
             // LoginScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.ClientSize = new System.Drawing.Size(417, 471);
             this.Controls.Add(this.LoginBtn);
             this.Controls.Add(this.PasswordBox);
--- TutorManagementSystem/LoginScreen.cs
+++ TutorManagementSystem/LoginScreen.cs
@@ -21,5 +21,10 @@ namespace TutorManagementSystem
         {
 
         }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
